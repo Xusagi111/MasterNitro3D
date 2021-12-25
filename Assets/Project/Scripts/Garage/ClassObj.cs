15d68a7649db2ae44ac2c70188d35b41492a8aba
Xusagi <xusagi.c.y@mail.ru>
2021-12-25 13:53:14 +0300

    DebagGarage

--- Assets/Project/Scripts/Garage/ClassObj.cs
+++ Assets/Project/Scripts/Garage/ClassObj.cs
@@ -4,58 +4,120 @@ using UnityEngine;
 
 public class ClassObj : MonoBehaviour
 {
-    [SerializeField] public GameObject[] gameObjects = new GameObject[6];
-    ClassObj classObj;
     int count = 0;
     [SerializeField] GameObject inite;
+    [SerializeField] GameObject CarSevDesc;
     [SerializeField] GameObject[] CarDes = new GameObject[6];
-    Vector3 vector3Car = new Vector3(4f, 4f, 4f);
-    Quaternion quaternionCar = Quaternion.Euler(0, -53, 0);
+    [SerializeField] GameObject[] gameObjects = new GameObject[6];
+    //Vector3 vector3Car = new Vector3(1f, 1f, 1f);
+    //Quaternion quaternionCar = Quaternion.Euler(0, -53, 0);
     private void Start()
     {
-        inite = Instantiate(gameObjects[count], gameObjects[count].transform.position, Quaternion.identity);
-        inite.transform.localScale = vector3Car;
-        inite.transform.rotation = quaternionCar;
+        inite = gameObjects[count];
         CarDes[count] = inite;
+        inite.SetActive(true);
     }
     public void LeftSwitchbutton()
     {
         int a = gameObjects.Length;
-        if(count != 0) 
-            count--;
+        //if (count != 0)
+        //    count--;
 
-        if (count > 0)
+        if (count != 0)
         {
-            inite = Instantiate(gameObjects[count-1], gameObjects[count-1].transform.position, Quaternion.identity);
-            inite.transform.localScale = vector3Car;
-            inite.transform.rotation = quaternionCar;
-            CarDes[count-1] = inite;
-            DestroyObject(CarDes[count]);
-            Debug.Log(count  + "count > 0");
-            
+            if (count == 0)
+            {
+                inite = gameObjects[count];
+                CarDes[count] = inite;
+                CarSevDesc = CarDes[count+1];
+                CarSevDesc.SetActive(false);
+                inite.SetActive(true);
+                Debug.Log(count + "count == 0");
+            }
+            if (count > 0)
+            {
+                inite = gameObjects[count - 1];
+                CarDes[count - 1] = inite;
+                CarSevDesc = CarDes[count];
+                CarSevDesc.SetActive(false);
+                inite.SetActive(true);
+                Debug.Log(count + "count > 0");
+            }
+            count--;
         }
 
     }
+    bool b = false;
     public void RightSwitchbutton()
     {
-        
         int a = gameObjects.Length;
-        if(count !=a)
+        if (count != a-1)
             count++;
-        Debug.Log(a + "A после сложения ");
-        Debug.Log(count + "коунт после сложения");
-
+        //Debug.Log(a + "A после сложения ");
+        //Debug.Log(count + "коунт после сложения");
+        
         if (count < gameObjects.Length)
         {
-
-            inite = Instantiate(gameObjects[count], gameObjects[count].transform.position, Quaternion.identity);
-            inite.transform.localScale = vector3Car;
-            inite.transform.rotation = quaternionCar;
+            inite = gameObjects[count];
             CarDes[count] = inite;
-           
-            DestroyObject(CarDes[count - 1]);
+            CarSevDesc = CarDes[count-1];
+            CarSevDesc.SetActive(false);
+            inite.SetActive(true);
             Debug.Log(count);
-            
         }
     }
+    //[SerializeField] public GameObject[] gameObjects = new GameObject[6];
+    //ClassObj classObj;
+    //int count = 0;
+    //[SerializeField] GameObject inite;
+    //[SerializeField] GameObject[] CarDes = new GameObject[6];
+    //Vector3 vector3Car = new Vector3(1f, 1f, 1f);
+    //Quaternion quaternionCar = Quaternion.Euler(0, -53, 0);
+    //private void Start()
+    //{
+    //    inite = Instantiate(gameObjects[count], gameObjects[count].transform.position, Quaternion.identity);
+    //    inite.transform.localScale = vector3Car;
+    //    inite.transform.rotation = quaternionCar;
+    //    CarDes[count] = inite;
+    //}
+    //public void LeftSwitchbutton()
+    //{
+    //    int a = gameObjects.Length;
+    //    if(count != 0) 
+    //        count--;
+
+    //    if (count > 0)
+    //    {
+    //        inite = Instantiate(gameObjects[count-1], gameObjects[count-1].transform.position, Quaternion.identity);
+    //        inite.transform.localScale = vector3Car;
+    //        inite.transform.rotation = quaternionCar;
+    //        CarDes[count-1] = inite;
+    //        DestroyObject(CarDes[count]);
+    //        Debug.Log(count  + "count > 0");
+
+    //    }
+
+    //}
+    //public void RightSwitchbutton()
+    //{
+
+    //    int a = gameObjects.Length;
+    //    if(count !=a)
+    //        count++;
+    //    Debug.Log(a + "A после сложения ");
+    //    Debug.Log(count + "коунт после сложения");
+
+    //    if (count < gameObjects.Length)
+    //    {
+
+    //        inite = Instantiate(gameObjects[count], gameObjects[count].transform.position, Quaternion.identity);
+    //        inite.transform.localScale = vector3Car;
+    //        inite.transform.rotation = quaternionCar;
+    //        CarDes[count] = inite;
+
+    //        DestroyObject(CarDes[count - 1]);
+    //        Debug.Log(count);
+
+    //    }
+    //}
 }
