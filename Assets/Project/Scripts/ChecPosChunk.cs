551662ac1e71c97003cd215170bebbcb63ca25eb
Xusagi <xusagi.c.y@mail.ru>
2022-01-27 15:39:18 +0300

    Added_New_Lvl_Scene_Test1

--- Assets/Project/Scripts/ChecPosChunk.cs
+++ Assets/Project/Scripts/ChecPosChunk.cs
@@ -6,29 +6,31 @@ public class ChecPosChunk : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.TryGetComponent<Chunk>(out Chunk chunk))
+        if (other.gameObject.TryGetComponent<Chunk>(out Chunk chunk)) //(other.gameObject.name == "Road_Straight" || other.gameObject.name == "Road_Right_Turn" )//(other.gameObject.TryGetComponent<Chunk>(out Chunk chunk))
         {
-            if (gameObject.GetComponent<MeshRenderer>() != false)
+            if (gameObject.GetComponent<MeshRenderer>() != false) //(gameObject.GetComponent<MeshRenderer>() != false)
             {
+                //Debug.Log("1"); 
                 gameObject.GetComponent<MeshRenderer>().enabled = false;
-                if(GetComponent<BoxCollider>() != null)
-                {
-                    gameObject.GetComponent<BoxCollider>().enabled = false;
-                }
-                else if (GetComponent<MeshCollider>() != null)
-                {
-                    gameObject.GetComponent<MeshCollider>().enabled = false;
-                }
+                //if(GetComponent<BoxCollider>() != null)
+                //{
+                //    gameObject.GetComponent<BoxCollider>().enabled = false;
+                //}
+                //else if (GetComponent<MeshCollider>() != null)
+                //{
+                //    gameObject.GetComponent<MeshCollider>().enabled = false;
+                //}
             } 
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.TryGetComponent<Chunk>(out Chunk chunk))
+        if(other.gameObject.TryGetComponent<Chunk>(out Chunk chunk))//(other.gameObject.name == "Road_Straight" || other.gameObject.name == "Road_Right_Turn")//(other.gameObject.TryGetComponent<Chunk>(out Chunk chunk))
         {
             if (gameObject.GetComponent<MeshRenderer>().enabled == false)
             {
                 gameObject.GetComponent<MeshRenderer>().enabled = true;
+                //Debug.Log("2");
             }
         }
             
