f0cd4f9817c22ec77e2e1539711ee62dfa203f48
Xusagi <xusagi.c.y@mail.ru>
2022-01-29 23:35:08 +0300

    Fix_box_collider_x

--- Assets/Project/Scripts/ChecPosChunk.cs
+++ Assets/Project/Scripts/ChecPosChunk.cs
@@ -6,7 +6,7 @@ public class ChecPosChunk : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.TryGetComponent<Chunk>(out Chunk chunk)) //(other.gameObject.name == "Road_Straight" || other.gameObject.name == "Road_Right_Turn" )//(other.gameObject.TryGetComponent<Chunk>(out Chunk chunk))
+        if (other.gameObject.GetComponent<Chunk>()) //(other.gameObject.name == "Road_Straight" || other.gameObject.name == "Road_Right_Turn" )//(other.gameObject.TryGetComponent<Chunk>(out Chunk chunk))
         {
             if (gameObject.GetComponent<MeshRenderer>() != false) //(gameObject.GetComponent<MeshRenderer>() != false)
             {
