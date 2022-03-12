ec42a439c7b5e82bedb1acdc7ff9f232356a3ccd
Xusagi <xusagi.c.y@mail.ru>
2022-03-12 15:32:53 +0300

    Added New Google LIst Gifts

---
+++ Assets/2_ProgrammData/Code/Scripts/InAppPurchases/Gifts_Google List/DataGifstTransferUsingGoogleSheet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+public class DataGifstTransferUsingGoogleSheet: MonoBehaviour 
+{
+    public static event Action<GiftsStatsToList, int[]> EventData;
+    [SerializeField] private string _sheetId;
+    public GiftsStatsToList _data;
+    private int[] IndexProduct;
+
+    private LoadUrlGoogleList _loadUrlGoogleList;
+    private HeaderListGifts  _headerListGifts;
+
+    private void Start()
+    {
+        _loadUrlGoogleList = GetComponent<LoadUrlGoogleList>();
+        _headerListGifts = GetComponent<HeaderListGifts>();
+        DownloadTable();
+    }
+
+    private void DownloadTable()
+    {
+        _loadUrlGoogleList.DownloadTable(_sheetId, OnRawCVSLoaded);
+    }
+
+    private void OnRawCVSLoaded(string rawCVSText)
+    {
+        (_data, IndexProduct) = (_headerListGifts.ProcessData(rawCVSText));
+        Debug.Log(EventData);
+        EventData?.Invoke(_data, IndexProduct); //вернул отсортированный список 
+    }
+}
+
---
+++ Assets/2_ProgrammData/Code/Scripts/InAppPurchases/Gifts_Google List/HeaderListGifts.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HeaderListGifts: MonoBehaviour
+{
+    #region Const
+    private const int _id = 0;
+    private const int _nameOffer = 1;
+    private const int _countCoint = 2;
+    private const int _countDiamons = 3;
+
+    private const char _cellSeporator = ',';
+    private const char _inCellSeporator = ';';
+    #endregion
+    private int _indexIdProduct;
+    private int dataStartRawIndex = 1;
+    private int[] indexId = new int[3];
+
+    public (GiftsStatsToList, int[] IndexId) ProcessData(string cvsRawData) //logic filling in existing machine statistics
+    {
+        char lineEnding = GetPlatformSpecificLineEnd();
+        string[] rows = cvsRawData.Split(lineEnding);
+        GiftsStatsToList data = new GiftsStatsToList();
+        data.ListCifts = new List<Cifts>();
+
+        for (int i = dataStartRawIndex; i < rows.Length; i++)
+        {
+            string[] cells = rows[i].Split(_cellSeporator);
+            int id = ParseInt(cells[_id]);
+            string NameOffer = cells[_nameOffer];
+            int CountCoint = ParseInt(cells[_countCoint]);
+            int CountDiamons = ParseInt(cells[_countDiamons]);
+
+            if (id != 0 && _indexIdProduct != ParseInt(cells[_id]))
+            {
+                _indexIdProduct = id;
+                for (int b = 0; b < indexId.Length; b++)
+                {
+                    if (indexId[b] == 0)
+                    {
+                        indexId[b] = _indexIdProduct;
+
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                id = _indexIdProduct;
+            }
+            if (NameOffer.ToString() != "" && CountCoint != 0)
+            {
+                data.ListCifts.Add(new Cifts()
+                {
+                    IndexKey = id,
+                    NameOffer = NameOffer,
+                    CountMoney = CountCoint,
+                    CountDiamons = CountDiamons,
+                });
+            }
+        }
+        return (data, indexId);
+    }
+    #region DopLogic
+    private int ParseInt(string s)
+    {
+        int result = -1;
+        if (!int.TryParse(s, System.Globalization.NumberStyles.Integer, System.Globalization.CultureInfo.GetCultureInfo("en-US"), out result))
+        {
+            //Debug.Log("Can't parse int, wrong text");
+        }
+
+        return result;
+    }
+
+    private float ParseFloat(string s)
+    {
+        float result = -1;
+        if (!float.TryParse(s, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.GetCultureInfo("en-US"), out result))
+        {
+            //Debug.Log("Can't pars float,wrong text ");
+        }
+
+        return result;
+    }
+
+    private char GetPlatformSpecificLineEnd()
+    {
+        char lineEnding = '\n';
+#if UNITY_IOS
+        lineEnding = '\r';
+#endif
+        return lineEnding;
+    }
+    #endregion
+
+}
+
