ea2a2e6d47df4955ef3cb07e3538f61924386ed5
Xusagi <xusagi.c.y@mail.ru>
2022-06-26 17:04:25 +0300

    Added New UI Main Menu

---
+++ Assets/2_ProgrammData/Code/Scripts/MainMenuGarage/UpdatePurchasesPanel.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.MainMenuGarage
+{
+    public class UpdatePurchasesPanel : MonoBehaviour
+    {
+        [field: SerializeField] private GameObject[] MoneyPanel { get; set; }
+        [field: SerializeField] private GameObject[] DiamondPanel { get; set; }
+        [field: SerializeField] private Button SwitchingMoney { get; set; }
+        [field: SerializeField] private Button SwitchingDiamond { get; set; }
+
+        private void Awake()
+        {
+            SwitchingMoney.onClick.AddListener(() => SetActivePanel(true, false));
+            SwitchingDiamond.onClick.AddListener(() => SetActivePanel(false, true));
+        }
+        public void StartPanel(bool isMoney)
+        {
+            if (isMoney)
+                SetActivePanel(true, false);
+            else
+
+                SetActivePanel(false, true);
+        }
+        private void SetActivePanel(bool isMoneyPanel, bool isDiamondPanel)
+        {
+            foreach (var item in MoneyPanel)
+            {
+                item.SetActive(isMoneyPanel);
+            }
+            foreach (var item in DiamondPanel)
+            {
+                item.SetActive(isDiamondPanel);
+            }
+        }
+    }
+}
